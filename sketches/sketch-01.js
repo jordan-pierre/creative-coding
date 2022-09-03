3a34efaead36136b7ff7efa4819a84fb7d8988fb
Jordan Pierre <41307129+jordan-pierre@users.noreply.github.com>
2022-09-03 19:07:42 -0400

    Parameterized grid canvas sketch

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -3,25 +3,31 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [600, 600]
+  dimensions: [1080, 1080]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
     // Make a grid of rectangles with some outlined
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.10;
+    const h = height * 0.10;
+    const gap = width * 0.03;
+    const init_x = width * 0.17;
+    const init_y = height * 0.17;
+
+    const offset = width * 0.02;
+
     let x, y;
     let grid = 5;
 
     for (let i = 0; i < grid; i++) {
       for (let j = 0; j < grid; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = init_x + (w + gap) * i;
+        y = init_y + (h + gap) * j;
 
         context.beginPath();
         context.rect(x, y, w, h);
@@ -29,7 +35,7 @@ const sketch = () => {
 
         if (Math.random() < 0.5) {
           context.beginPath();
-          context.rect(x + 8, y + 8, w - 16, h - 16);
+          context.rect(x + offset / 2, y + offset / 2, w - offset, h - offset);
           context.stroke();
         }
       }
